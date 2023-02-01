cfcfaf1a808f34bc94aee175b50f7320aa5cac20
Hemraj Shahi <you@example.com>
2023-02-01 14:22:20 -0500

    added findfactorial

---
+++ findFactorial.js
@@ -0,0 +1,13 @@
+/**
+ * Find factorial of a number using 'Recursive' function.
+ * The factorial of a number is the product of all the integers from 1 to that number. For example, the factorial of 6 is 1*2*3*4*5*6 = 720 .
+ */
+
+function findFactorial(number) {
+    factorial = number*(number -1)*;
+    
+    if (number >=1){
+        findFactorial(number);
+    };
+
+}
