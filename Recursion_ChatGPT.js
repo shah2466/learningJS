b9bacff7638c36530045ea303e867d63133ba793
shah2466 <hemraj.shahi90@gmail.com>
2023-03-16 15:56:37 -0400

    Create Recursion_ChatGPT.js

---
+++ Recursion_ChatGPT.js
@@ -0,0 +1,31 @@
+// Recursion is a powerful programming technique in which a function calls itself repeatedly until it reaches a base case that triggers the function to stop calling itself.
+// In JavaScript, recursion can be used to solve problems that have a recursive structure, such as traversing trees, searching through linked lists, and many 
+// other algorithms. Here's a basic example of a recursive function in JavaScript that computes the factorial of a number:
+
+function factorial(n) {
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n-1);
+  }
+}
+// In this example, the factorial function takes an argument n and checks if it's equal to 0. If it is, it returns 1 (the base case). If n is not 0, 
+// it recursively calls itself with n-1 as the argument, and multiplies the result of that call by n.
+// When you call factorial(4), for example, the function calls itself four times:
+
+factorial(4)
+  factorial(3)
+    factorial(2)
+      factorial(1)
+        factorial(0)
+// At this point, the base case is triggered and the function starts returning values back up the call stack:
+
+factorial(0) returns 1
+factorial(1) returns 1 * 1 = 1
+factorial(2) returns 2 * 1 = 2
+factorial(3) returns 3 * 2 = 6
+factorial(4) returns 4 * 6 = 24
+And the final result is 24, the factorial of 4.
+
+// It's important to note that recursive functions can be very powerful, but they can also be inefficient if not implemented properly. 
+// It's important to carefully consider the base case and ensure that the function is not calling itself excessively or unnecessarily
